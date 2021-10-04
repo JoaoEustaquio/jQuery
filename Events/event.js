b8b6ad6533e7799f374449d03c0fc669a7e6dc6f
Joao_Godinho <git.qodeless@outlook.com>
2021-10-04 09:28:53 -0300

    Fix error

--- Events/event.js
+++ Events/event.js
@@ -1,29 +1,27 @@
-$(function(){
-    /*
-    $('img').error(function(){
-         $('img').attr("src","img/image-error.png");
-    });
+$(function() {
+//   $('img').error(function () {
+//     $("img").attr("src", "img/error.png");
+//   });
 
-    $('img').error(function(){
-         var imagem = $(this).attr("src");
-         alert('Imagem' +imagem+ 'Indisponivel!');
-         $('img').attr("src","img/error.png");
-     });
-     */
+    // $('img').error(function(){
+    //      var imagem = $(this).attr("src");
+    //      alert('Imagem' +imagem+ 'Indisponivel!');
+    //      $('img').attr("src","img/error.png");
+    //  });
+     
+//   $('img').width($(window).width()).height($(window).height());
+//   $(window).resize(function(){
+//       $('img').width($(window).width()).height($(window).height());
+//   })
 
-    // $('img').width($(window).width()).height($(window).height());
-    // $(window).resize(function(){
-    //     $('img').width($(window).width()).height($(window).height());
-    // })
+  $('body').css("height", "5000px");
 
-    $('body').css("height", "5000px");
-
-    $(window).scroll(function(){
-        var topo = $(window).scrollTop();
-        if(topo>400){
-            $('img').fadeOut('1000');
-        }else{
-            $('img').fadeIn('1000');
-        }
-    });
+  $(window).scroll(function(){
+      var topo = $(window).scrollTop();
+      if(topo>50){
+          $('img').fadeOut('1000');
+      }else{
+          $('img').fadeIn('1000');
+      }
+  });
 });
