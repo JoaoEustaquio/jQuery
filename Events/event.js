15851d5c366d0e80463750a459b907dd978074ef
Joao_Godinho <git.qodeless@outlook.com>
2021-10-04 10:49:01 -0300

    Mouse Events

--- Events/event.js
+++ Events/event.js
@@ -1,27 +1,29 @@
-$(function() {
-//   $('img').error(function () {
-//     $("img").attr("src", "img/error.png");
-//   });
+$(function () {
+  $("body").css("height", "5000px");
 
-    // $('img').error(function(){
-    //      var imagem = $(this).attr("src");
-    //      alert('Imagem' +imagem+ 'Indisponivel!');
-    //      $('img').attr("src","img/error.png");
-    //  });
-     
-//   $('img').width($(window).width()).height($(window).height());
-//   $(window).resize(function(){
-//       $('img').width($(window).width()).height($(window).height());
-//   })
-
-  $('body').css("height", "5000px");
-
-  $(window).scroll(function(){
-      var topo = $(window).scrollTop();
-      if(topo>50){
-          $('img').fadeOut('1000');
-      }else{
-          $('img').fadeIn('1000');
-      }
+  $(window).scroll(function () {
+    var topo = $(window).scrollTop();
+    if (topo > 50) {
+      $("img").fadeOut("1000");
+    } else {
+      $("img").fadeIn("1000");
+    }
   });
 });
+
+function justToSave() {
+  $("img").error(function () {
+    $("img").attr("src", "img/error.png");
+  });
+
+  $("img").error(function () {
+    var imagem = $(this).attr("src");
+    alert("Imagem" + imagem + "Indisponivel!");
+    $("img").attr("src", "img/error.png");
+  });
+
+  $("img").width($(window).width()).height($(window).height());
+  $(window).resize(function () {
+    $("img").width($(window).width()).height($(window).height());
+  });
+}
---
+++ MouseEvents/js/aula6.js
@@ -0,0 +1,57 @@
+$(function(){
+
+        var ex = $('.ex1');
+
+    $('.ev1').click(function(){
+        $(this).css("background", "#ccc");
+        ex.text("Você Clicou!");
+    });
+
+    $('.ev2').dblclick(function(){
+        $(this).css("background", "#ccc");
+        ex.text("Você deu dois Cliques!");
+    });
+
+    $('.ev3').focusin(function(){
+        $(this).css("background", "#ccc");
+        ex.text("Você deu Foco!");
+    }).focusout(function(){
+        $(this).css("background", "#000");
+        ex.text("Você tirou o Foco!")
+    });
+
+    $('.ev4').hover(function(){
+        $(this).css("background", "#ccc");
+        ex.text("Você Passou o Mouse!");
+    });
+
+    $('.ev5').mousedown(function(){
+        $(this).css("background", "#ccc");
+        ex.text("Você Apertou o botão do Mouse!");
+    }).mouseup(function(){
+        $(this).css("background", "#000");
+        ex.text("Você Soltou o Botão do Mouse!");
+    })
+
+    var a = 0;
+    $('.ev6').mouseenter(function(){
+        a += 1;
+        ex.text("Entradas do Mouse: " +a);
+    }).mouseout(function(){
+        ex.text("Saída do Mouse")
+    });
+
+    var b = 0;
+    $('.ev7').mouseover(function(){
+        b += 1;
+        ex.text("Mouse over: " + b);
+    }).mouseleave(function(){
+        ex.text("Mouse Leave")
+    });
+
+    $('.ev8').mousemove(function(move){
+        var localx = move.pageX;
+        var localy = move.pageY;
+        ex.text("Movimento X: " +localx+" -  Movimento Y: "+localy)
+    });
+});
